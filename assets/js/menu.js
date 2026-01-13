0094f667bd4434480f1e7c51082bfbf5523aeab7
Raven-G-19 <rpgadayan@antiquespride.edu.ph>
2026-01-13 18:44:44 +0800

    Organized file structure and added sidebar

---
+++ assets/js/menu.js
@@ -0,0 +1,9 @@
+function openMenu() {
+  document.getElementById("drawer").classList.add("open");
+  document.getElementById("overlay").classList.add("show");
+}
+
+function closeMenu() {
+  document.getElementById("drawer").classList.remove("open");
+  document.getElementById("overlay").classList.remove("show");
+}
